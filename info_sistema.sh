980902308a495457c8b889bc5c37ba84417625b2
Nicolas troitino <nicotrmail@gmail.com>
2026-05-22 20:00:19 +0200

    Añade script de información básica del sistema

---
+++ info_sistema.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Información básica del sistema"
+echo "-----------------------------"
+echo "Nombre del equipo:"
+hostname
+
+echo
+echo "Usuario actual:"
+whoami
+
+echo
+echo "Fecha y hora actual:"
+date
